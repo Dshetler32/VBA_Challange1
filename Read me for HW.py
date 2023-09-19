1dfd66e4d4feb3bd762a0e81e297c72bfc6db6a6
Dshetler32 <140770090+Dshetler32@users.noreply.github.com>
2023-09-18 22:31:39 -0500

    Stock data xl screenshots and VBA Code and readme

---
+++ Read me for HW.py
@@ -0,0 +1,4 @@
+  # on this assignment I worked with many different sources
+  # I worked with both a tutor and the Chat tutor on slack
+  # I worked with My roomate William
+  # for anything else i couldnt figure out i used google to search for specific examples of functions 
